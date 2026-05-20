731af5ec1b669aeef4179a99e7e5f22af78d686f
kimurim630 <kimurim96@gmail.com>
2026-05-21 02:12:22 +0900

    add html to index.php

--- index.php
+++ index.php
@@ -4,3 +4,30 @@ $sql = "SELECT * FROM posts ORDER BY id DESC";
 $result = mysqli_query($conn,$sql);
 ?>
 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>게시판</title>
+    </head>
+    <body>
+        <h1>게시판</h1>
+        <a href="write.php">글쓰기</a>
+        <hr>
+
+        <?php
+        -- 게시글 출력
+        while ($row = mysqli_fetch_assoc($result))
+        {
+        ?>
+            <h3><?php echo $row['title'];></h3>
+            <p><?php ehco $row['content'];></p>
+            <small><?php ehco $row['created_at'];></small>
+            <hr>
+        <?php
+        }
+        ?>
+    </body>
+</html>
+
+
